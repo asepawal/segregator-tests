d7bbfb34f1ed976b0beea34ad6193d131d0d817c
Asep Muhamad Awaludin <asep.muhamad11@gmail.com>
2020-05-08 00:50:05 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Segregator - Evaluation Repository
+This repository is submodule of [segregator-riscv](https://github.com/asepawal/segregator-riscv.git) and cannot work as stand alone.
---
+++ benchmarks/run-benchmark.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Compile and run regregator benchmark
+
+# Fail on error
+set -e
+
+source ../../config.common
+
+BENCHMARKS=($(ls -d beebs/src/*/ | xargs -n 1 basename))
+
+EXCLUDE_BENCHMARKS=(ctl matmult sglib-arraysort trio)
+
+# memmove in RTOS TODO?
+#EXCLUDE_BENCHMARKS+=(ctl-string ctl-vector cubic dtoa fdct matmult-float frac miniz nbody nettle-aes nettle-sha256 qrduino slre st stb_perlin stringsearch1 trio-snprintf trio-sscanf whetstone wikisort)
+
+# I dont know why it fails in RTOS TODO
+#EXCLUDE_BENCHMARKS+=(cnt crc32 duff fasta huffbench insertsort lcdnum levenshtein ludcmp mergesort minver ndes nettle-arcfour nettle-cast128 nettle-md5 newlib-exp newlib-log newlib-mod newlib-sqrt picojpeg qsort qurt recursion rijndael select sglib-arraysort sglib-listsort sglib-queue sglib-rbtree statemate strstr tarai) 
+
+for i in "${EXCLUDE_BENCHMARKS[@]}"; do
+    DISABLE_BENCHMARKS="$DISABLE_BENCHMARKS --disable-benchmark-$i"
+done
+
+if [[ $RISCV_KERNEL = "freertos" ]]; then
+    echo "Using FreeRTOS"
+    CHIP=freertos
+    BOARD=freertos
+    SPIKE="spike"
+else
+    echo "Using Proxy Kernel"
+    CHIP=generic
+    BOARD=none
+    SPIKE="spike pk"
+fi
+
+rm -rf results/*.elf
+rm -rf results/*.out
+
+# compile beebs
+cd beebs
+
+#baseline
+./configure --host=riscv64-unknown-elf --with-chip=$CHIP --with-board=$BOARD $DISABLE_BENCHMARKS
+make clean
+make
+for i in "${BENCHMARKS[@]}"; do
+    if [[ ${EXCLUDE_BENCHMARKS[*]} =~ $i ]]; then
+        continue
+    fi
+
+    cp src/$i/$i ../results/$i.elf 
+    echo -n "Executing $i.."
+    ${SPIKE} ../results/$i.elf > ../results/$i.out || true
+    echo "done"
+done
+
+#segregator TODO
+
+#segregator + security TODO
+
+cd - > /dev/null
+echo "DONE"
